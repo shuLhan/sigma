23016472124f3b5fb21c37901599c5033d00e1d7
Mhd Sulhan <m.shulhan@gmail.com>
2014-09-06 23:12:36 +0700

    [sigma] Customer.Payment: new module.
    * Move Customer/Profile/layout.js to Customer/layout.js.

---
+++ module/Customer/Payment/index.php
@@ -0,0 +1,43 @@
+<?php
+/*
+	Copyright 2014 Mhd Sulhan
+	Authors:
+		- mhd.sulhan (m.shulhan@gmail.com)
+*/
+require_once "../../init.php";
+
+$fields	= [
+	"id"
+,	"_user_id"
+,	"ts"
+
+,	"customer_id"
+,	"pay_date"
+,	"payment_lot_id"
+,	"payment_type_id"
+
+,	"bank_source_id"
+,	"bank_dest_id"
+
+,	"receipt_code"
+,	"total"
+];
+
+Jaring::$_mod["db_table"]["name"]		= "customer_payment";
+Jaring::$_mod["db_table"]["read"]		= $fields;
+Jaring::$_mod["db_table"]["search"]		= ["receipt_code"];
+Jaring::$_mod["db_table"]["order"]		= ["pay_date DESC"];
+Jaring::$_mod["db_table"]["create"]		= $fields;
+Jaring::$_mod["db_table"]["update"]		= array_slice ($fields, 3);
+Jaring::$_mod["db_table"]["profiled"]	= false;
+
+function request_create_before (&$data)
+{
+	foreach ($data as &$d) {
+		$d["_user_id"] = Jaring::$_c_uid;
+		$d["ts"] = time ();
+	}
+	return true;
+}
+
+Jaring::request_handle ("crud");
---
+++ module/Customer/layout.js
@@ -0,0 +1,623 @@
+/*
+	Copyright 2014 - Mhd Sulhan
+	Authors:
+		- mhd.sulhan (m.shulhan@gmail.com)
+*/
+//{{{ Panel: customer payment.
+function Jx_Customer_Payment ()
+{
+	var self = this;
+
+	this.id = "Customer_Payment";
+	this.dir = Jx.generateModDir (this.id);
+	this.perm = 0;
+
+	//{{{ panel
+	this.panel = Ext.create ("Jx.CardGridForm",
+	{
+		itemId		:this.id
+	,	url			:this.dir
+	,	title		:"Pembayaran"
+	,	region		:"east"
+	,	split		:true
+	,	width		:"50%"
+	,	closable	:false
+	,	fields		:
+		[{
+			header		:"ID"
+		,	dataIndex	:"id"
+		,	hidden		:true
+		,	editor		:
+			{
+				hidden		:true
+			}
+		},{
+			header		:"Customer ID"
+		,	dataIndex	:"customer_id"
+		,	hidden		:true
+		,	editor		:
+			{
+				hidden		:true
+			}
+		},{
+			header		:"Tanggal"
+		,	width		:100
+		,	dataIndex	:"pay_date"
+		,	xtype		:"datecolumn"
+		,	format		:"Y-m-d"
+		,	editor		:
+			{
+				xtype		:"datefield"
+			,	format		:"Y-m-d"
+			,	allowBlank	:false
+			,	value		:new Date ()
+			}
+		},{
+			header		:"Untuk"
+		,	dataIndex	:"payment_lot_id"
+		,	renderer	:Jx.app.store.References.Payment.Lot.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combobox"
+			,	store			:Jx.app.store.References.Payment.Lot
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			,	allowBlank		:false
+			}
+		},{	
+			header		:"Cara"
+		,	dataIndex	:"payment_type_id"
+		,	width		:160
+		,	renderer	:Jx.app.store.References.Payment.Type.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combobox"
+			,	itemId			:"payment_type_id"
+			,	store			:Jx.app.store.References.Payment.Type
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			,	allowBlank		:false
+			}
+		},{
+			header		:"Bank Sumber"
+		,	dataIndex	:"bank_source_id"
+		,	renderer	:Jx.app.store.References.Bank.renderData ("id", "name")
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype			:"combobox"
+			,	itemId			:"bank_source_id"
+			,	store			:Jx.app.store.References.Bank
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		},{
+			header		:"Bank Tujuan"
+		,	dataIndex	:"bank_dest_id"
+		,	renderer	:Jx.app.store.References.Bank.renderData ("id", "name")
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype			:"combobox"
+			,	itemId			:"bank_dest_id"
+			,	store			:Jx.app.store.References.Bank
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		},{
+			header		:"Kode Transaksi / No. Kwitansi"
+		,	dataIndex	:"receipt_code"
+		,	width		:160
+		,	editor		:{}
+		},{
+			header		:"Jumlah"
+		,	dataIndex	:"total"
+		,	flex		:1
+		,	editor		:
+			{
+				xtype		:"numberfield"
+			,	allowBlank	:false
+			}
+		}]
+	});
+	//}}}
+	this.cb_payment_type_id = this.panel.form.down ("#payment_type_id");
+	this.f_bank_src = this.panel.form.down ("#bank_source_id");
+	this.f_bank_dst = this.panel.form.down ("#bank_dest_id");
+
+	this.cb_payment_type_id.on ("change", function (cmp, newv, oldv)
+	{
+		switch (newv) {
+		case 1: // cash
+			this.f_bank_src.setDisabled (true);
+			this.f_bank_dst.setDisabled (true);
+			break;
+		case 2: // transfer
+			this.f_bank_src.setDisabled (false);
+			this.f_bank_dst.setDisabled (false);
+			break;
+		case 3:	// debit
+			this.f_bank_src.setDisabled (false);
+			this.f_bank_dst.setDisabled (true);
+			break;
+		}
+	}, this);
+
+	//{{{ f : doRefresh.
+	this.doRefresh = function (perm, cust_id)
+	{
+		this.perm = perm;
+
+		if (cust_id == 0) {
+			self.panel.grid.clearData ();
+		} else {
+			self.panel.grid.store.proxy.extraParams.customer_id = cust_id;
+			self.panel.grid.doRefresh (perm);
+		}
+	};
+	//}}}
+}
+//}}}
+//{{{ Panel: Customer Profile.
+function Jx_Customer_Profile ()
+{
+	var self = this;
+
+	this.id = "Customer_Profile";
+	this.dir = Jx.generateModDir (this.id);
+	this.perm = 0;
+	this.payment = new Jx_Customer_Payment ();
+
+	//{{{ fieldset: profile.
+	this.fs_profile =
+		{
+			header		:"Kostumer Profil"
+		,	fsConfig	:
+			{
+				padding		:10
+			}
+		,	columns	:
+			[{
+				header		:"ID"
+			,	dataIndex	:"id"
+			,	hidden		:true
+			,	editor		:
+				{
+					hidden		:true
+				}
+			},{
+				header		:"Nama"
+			,	dataIndex	:"name"
+			,	width		:200
+			,	editor		:
+				{
+					allowBlank	:false
+				}
+			},{
+				header		:"Telepon/HP"
+			,	dataIndex	:"phone"
+			,	editor		:
+				{
+					xtype			:"numberfield"
+				,	allowBlank		:false
+				,	allowDecimals	:false
+				,	hideTrigger		:true
+				}
+			},{
+				header		:"Alamat"
+			,	dataIndex	:"address"
+			,	hidden		:true
+			,	editor		:
+				{
+					xtype		:"textarea"
+				}
+			},{
+				hidden		:true
+			,	editor		:this.college_or_company
+			}]
+		};
+
+	//}}}
+	//{{{ radiogroup: college or company
+	this.college_or_company = Ext.create ("Ext.form.RadioGroup",
+		{
+			xtype	:"radiogroup"
+		,	columns	:1
+		,	items	:
+			[{
+				boxLabel	:"Perguruan Tinggi"
+			,	name		:"college_or_company"
+			,	inputValue	:1
+			,	checked		:true
+			},{
+				boxLabel	:"Perusahaan"
+			,	name		:"college_or_company"
+			,	inputValue	:2
+			}]
+		});
+	//}}}
+	//{{{ fieldset: college.
+	this.fs_college =
+	{
+		header		:"Perguruan Tinggi"
+	,	fsConfig	:
+		{
+			padding		:10
+		,	itemId		:"fs_college"
+		}
+	,	columns		:
+		[{
+			header		:"Nama"
+		,	dataIndex	:"college_id"
+		,	renderer	:Jx.app.store.References.College.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combo"
+			,	store			:Jx.app.store.References.College
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		},{
+			header		:"Fakultas"
+		,	dataIndex	:"college_faculty_id"
+		,	hidden		:true
+		,	renderer	:Jx.app.store.References.College.Faculty.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combo"
+			,	store			:Jx.app.store.References.College.Faculty
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		},{
+			header		:"Jurusan"
+		,	dataIndex	:"college_major_id"
+		,	hidden		:true
+		,	renderer	:Jx.app.store.References.College.Major.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combo"
+			,	store			:Jx.app.store.References.College.Major
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		},{
+			header		:"Pendidikan"
+		,	dataIndex	:"college_degree_id"
+		,	width		:200
+		,	renderer	:Jx.app.store.References.College.Degree.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combo"
+			,	store			:Jx.app.store.References.College.Degree
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		}]
+	};
+	//}}}
+	//{{{ fieldset: company.
+	this.fs_company =
+	{
+		header		:"Perusahaan"
+	,	hidden		:true
+	,	fsConfig	:
+		{
+			padding		:10
+		,	disabled	:true
+		,	itemId		:"fs_company"
+		}
+	,	columns		:
+		[{
+			header		:"Nama"
+		,	dataIndex	:"company_name"
+		,	hidden		:true
+		,	editor		:{}
+		}]
+	};
+	//}}}
+	//{{{ fieldset: source information.
+	this.fs_sourceinfo = 
+	{
+		header		:"Sumber Informasi Mengetahui Sigma"
+	,	hidden		:true
+	,	fsConfig	:
+		{
+			padding		:10
+		}
+	,	columns		:
+		[{
+			dataIndex	:"source_info_id"
+		,	hidden		:true
+		,	renderer	:Jx.app.store.References.SourceInfo.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combo"
+			,	itemId			:"source_info_id"
+			,	store			:Jx.app.store.References.SourceInfo
+			,	valueField		:"id"
+			,	displayField	:"name"
+			,	editable		:false
+			}
+		},{
+			dataIndex	:"source_info_others"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textarea"
+			,	itemId		:"source_info_others"
+			,	hidden		:true
+			}
+		}]
+	};
+	//}}}
+	//{{{ fieldset: cost.
+	this.fs_cost =
+	{
+		header		:"Biaya"
+	,	fsConfig	:
+		{
+			padding		:10
+		}
+	,	columns		:
+		[{
+			header		:"Jumlah Responden"
+		,	dataIndex	:"n_respondent"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype			:"numberfield"
+			,	allowDecimals	:false
+			,	hideTrigger		:true
+			,	minValue		:0
+			}
+		},{
+			header		:"Jumlah Item"
+		,	dataIndex	:"n_item"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype			:"numberfield"
+			,	allowDecimals	:false
+			,	hideTrigger		:true
+			,	minValue		:0
+			}
+		},{
+			header		:"Analisis Data"
+		,	dataIndex	:"using_analysis_data"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"checkbox"
+			,	inputValue	:1
+			}
+		},{
+			header		:"Konsultasi"
+		,	dataIndex	:"using_consultation"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"checkbox"
+			,	inputValue	:1
+			}
+		},{
+			header		:"Biaya Lain #1"
+		,	dataIndex	:"cost_add_1"
+		,	hidden		:true
+		,	editor		:{}
+		},{
+			header		:"Nilai"
+		,	dataIndex	:"cost_add_value_1"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"numberfield"
+			}
+		},{
+			header		:"Biaya Lain #2"
+		,	dataIndex	:"cost_add_2"
+		,	hidden		:true
+		,	editor		:{}
+		},{
+			header		:"Nilai"
+		,	dataIndex	:"cost_add_value_2"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"numberfield"
+			}
+		},{
+			header		:"Biaya Lain #3"
+		,	dataIndex	:"cost_add_3"
+		,	hidden		:true
+		,	editor		:{}
+		},{
+			header		:"Nilai"
+		,	dataIndex	:"cost_add_value_3"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"numberfield"
+			}
+		},{
+			header		:"Total"
+		,	dataIndex	:"cost_total"
+		,	editor		:
+			{
+				xtype		:"displayfield"
+			}
+		}]
+	};
+	//}}}
+	//{{{ fieldset: payment.
+	this.fs_payment =
+	{
+		header		:"Pembayaran"
+	,	fsConfig	:
+		{
+			padding		:10
+		,	hidden		:true
+		}
+	,	columns		:
+		[{
+			header		:"DP 1"
+		,	dataIndex	:"payment_1"
+		,	hidden		:true
+		},{
+			header		:"DP 2"
+		,	dataIndex	:"payment_2"
+		,	hidden		:true
+		},{
+			header		:"DP 3"
+		,	dataIndex	:"payment_3"
+		,	hidden		:true
+		},{
+			header		:"Pelunasan"
+		,	dataIndex	:"payment_4"
+		,	hidden		:true
+		},{
+			header		:"Total"
+		,	dataIndex	:"payment_total"
+		}]
+	};
+	//}}}
+	//{{{ panel : profile.
+	this.panel = Ext.create ("Jx.CardGridForm",
+	{
+		itemId		:this.id
+	,	url			:this.dir
+	,	title		:"Profil"
+	,	region		:"center"
+	,	closable	:false
+	,	formConfig	:
+		{
+			layout		:"column"
+		,	defaults	:
+			{
+				margin		:"0 4 0 0"
+			}
+		,	fieldDefaults	:
+			{
+				labelAlign		:"right"
+			,	labelWidth		:120
+			,	width			:340
+			}
+		}
+	,	fields	:
+		[
+			this.fs_profile
+		,	this.fs_company
+		,	this.fs_college
+		,	this.fs_sourceinfo
+		,	this.fs_cost
+		,	this.fs_payment
+		]
+	});
+	//}}}
+	//{{{ components.
+	this._fs_sourceinfo = this.panel.form.down ("#source_info_id");
+	this._ta_source_others = this.panel.form.down ("#source_info_others");
+	//}}}
+	//{{{ radiogroup college_or_company on change.
+	this.college_or_company.on ("change", function (cmp, newv, oldv)
+	{
+		switch (newv.college_or_company) {
+		case 1:
+			self.panel.form.down ("#fs_college").setDisabled (false)
+			self.panel.form.down ("#fs_company").setDisabled (true);
+			break;
+		case 2:
+			self.panel.form.down ("#fs_college").setDisabled (true);
+			self.panel.form.down ("#fs_company").setDisabled (false);
+			break;
+		}
+	});
+	//}}}
+	//{{{ sourceinfo on change
+	this._fs_sourceinfo.on ("change", function (cmp, newv, oldv)
+	{
+		if (newv === 99) {
+			self._ta_source_others.show ();
+		} else {
+			self._ta_source_others.hide ();
+		}
+	});
+	//}}}
+	//{{{ f : doRefresh.
+	this.doRefresh = function (perm)
+	{
+		this.perm = perm;
+		this.panel.grid.doRefresh (perm);
+	};
+	//}}}
+}
+//}}}
+//{{{ Main
+function Jx_Customer ()
+{
+	var self = this;
+
+	this.id = "Customer";
+	this.dir = Jx.generateModDir (this.id);
+	this.perm = 0;
+	this.cust_profile = new Jx_Customer_Profile ();
+	this.cust_payment = new Jx_Customer_Payment ();
+
+	this.panel = Ext.create ("Ext.panel.Panel",
+	{
+		itemId		:"Customer"
+	,	title		:"Kostumer"
+	,	titleAlign	:"center"
+	,	closable	:true
+	,	layout		:"border"
+	,	items		:
+		[
+			this.cust_profile.panel
+		,	this.cust_payment.panel
+		]
+	});
+
+	this.doRefresh = function (perm)
+	{
+		this.perm = perm;
+
+		Jx.chainStoreLoad (
+			[
+				Jx.app.store.References.College
+			,	Jx.app.store.References.College.Faculty
+			,	Jx.app.store.References.College.Major
+			,	Jx.app.store.References.College.Degree
+			,	Jx.app.store.References.Payment.Type
+			,	Jx.app.store.References.Payment.Lot
+			,	Jx.app.store.References.Bank
+			]
+		,	function ()
+			{
+				self.cust_profile.doRefresh (perm);
+				self.cust_payment.doRefresh (perm, 0);
+			}
+		,	0);
+	}
+
+	this.cust_profile.panel.grid.afterSelectionChange = function (model, data)
+	{
+		if (data.length <= 0) {
+			return;
+		}
+		self.cust_payment.doRefresh (self.perm, data[0].get ("id"));
+	}
+}
+//}}}
+//}}}
+
+var Customer = new Jx_Customer ();
+
+//# sourceURL=module/Customer/layout.js
